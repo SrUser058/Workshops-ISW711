f8641248dc261a7a217631e3fa0e3969b3282b58
Eduardo Cordero <ecorderoc@est.utn.ac.cr>
2024-02-20 22:34:48 -0600

    Workshop#3 Done

---
+++ Workshop#3/server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+//const cors = require("cors");
+
+//Diferencia de usar bodyparse a solo express?
+//const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json());
+
+// ¿Es mejor usar funtion o =>?
+app.get('/hello/:messange', (req,res) => {
+    if(!req.params.messange) {
+        res.send("Hello World!")
+    }
+    res.send(`Hello ${req.params.messange}`)    
+})
+
+app.get('/', (req,res) => {
+    res.send("Conected to the Eduardo's Api")
+})
+
+app.post('/user', (req, res) => {
+    if(!req.body.name && !req.body.lastname){
+        res.send("Parametros no enviados")
+    }
+    res.send(`Usuario ${req.body.name + req.body.lastname} se conecto existosamente`)
+})
+
+app.listen(3001, () => console.log(`Service listening on port 3001!`))
